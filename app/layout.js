2fe22d2c0236f2ac5a22fa29c04771677cf11b5f
ronaldsrezais-cpu <ronalds.rezais@gmail.com>
2026-07-22 10:39:12 +0300

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body>{children}
+        <Analytics />
+      </body>
     </html>
   );
 }
