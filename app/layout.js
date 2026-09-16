041ce82a4906572f7d7ac3fd8cd9b4b06542ed9f
ronaldsrezais-cpu <ronalds.rezais@gmail.com>
2026-09-16 14:53:12 +0300

    Load automated festival map and activity updates

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <script src="/auto-updates.js" defer></script>
         {/* Meta Pixel Code */}
         <script
           id="meta-pixel-base"
